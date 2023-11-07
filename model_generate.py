b30530acc02b29ec90e463b3668eae9f67178866
v-xchen-v <407081305@qq.com>
2023-11-07 12:08:04 +0800

    model generate API

--- model_generate.py
+++ model_generate.py
@@ -1,8 +1,9 @@
+# https://huggingface.co/docs/transformers/generation_strategies
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 tokenizer = AutoTokenizer.from_pretrained('gpt2')
 model = AutoModelForCausalLM.from_pretrained('gpt2').to('cuda')
-# model.to('cuda')
+
 print(model.generation_config)
 text="Am I cool?" 
 encoded = tokenizer(text, return_tensors='pt').to('cuda')
